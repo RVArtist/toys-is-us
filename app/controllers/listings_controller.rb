7d59c1fe424a3cad9935c89eb64af280b7e7c99f
RVArtist <81139429+RVArtist@users.noreply.github.com>
2022-03-19 16:59:48 +1100

    storage and img

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -47,7 +47,7 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :price, :category_id, :condition, :description)
+    params.require(:listing).permit(:title, :price, :category_id, :condition, :description, :picture)
 
   end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,5 @@ class Listing < ApplicationRecord
 
   #conditions
   enum condition: {very_loved: 1, loved: 2, well_looked_after: 3, brand_new:4}
+  has_one_attached :picture
 end
